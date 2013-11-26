41c04416f7316145dfa5b3654a95bcdaeaad27ec
tjt7a <tjta@virginia.edu>
2013-11-26 12:28:18 -0500

    Updated Parent and Child application code

--- apps/router_fft_test_child.cc
+++ apps/router_fft_test_child.cc
@@ -18,20 +18,6 @@
  * Boston, MA 02110-1301, USA.
  */
 
-
-/*
- * GNU Radio C++ Gr-Router Example
- * This example uses a change of FFT/IFFTs for the test
- *
- * GNU Radio makes extensive use of Boost shared pointers.  Signal processing
- * blocks are typically created by calling a "make" factory function, which
- * returns an instance of the block as a typedef'd shared pointer that can
- * be used in any way a regular pointer can.  Shared pointers created this way
- * keep track of their memory and free it at the right time, so the user
- * doesn't need to worry about it (really).
- *
- */
-
 // Include header files for each block used in flowgraph
 #include <gnuradio/top_block.h>
 #include <gnuradio/blocks/wavfile_source.h>
@@ -50,36 +36,48 @@ using namespace gr;
 
 int main(int argc, char **argv)
 {
-   // Construct a top block that will contain flowgraph blocks.  Alternatively,
-  // one may create a derived class from top_block and hold instantiated blocks
-  // as member data for later manipulation.
 
+  /*
+  * Create a Top Block for the flowgraph
+  */
   gr::top_block_sptr tb = gr::make_top_block("fft_ifft_child");
 
   int window_size = 1024;
   int fft_count = 50;
 
+  /*
+  * Input and Output queues to hold 'windows' of 1025 floats. 1 index, and 1024 samples
+  */
 
-  // Lockfree queues for input and output windows
   boost::lockfree::queue< std::vector<float>* > input_queue(1025);
   boost::lockfree::queue< std::vector<float>* > output_queue(1025);
 
-  GR_LOG_INFO(d_logger, "1. Created input/output queues");
+  /*
+  * Load Balancing Router to receive windows from parent
+  */
 
   gr::router::child::sptr child_router = gr::router::child::make(0, 0, "localhost", input_queue, output_queue);
 
-  GR_LOG_INFO(d_logger, "2. Finished building child router");
+  /*
+  * Input queue Sink: Takes streams from a flow graph, packetizes, slaps on an index, and pushes the result into the input queue; last argument indicates if index is to be preserved from the stream tags
+  * Input queue Source: Grabs windows from the input queue, depacketizes, pulls out the index, and streams through flowgraph; arguments: preserve index (stream tags), order (guarantee order of windows before streaming)
+  */
 
   gr::router::queue_sink::sptr input_queue_sink = gr::router::queue_sink::make(sizeof(float), input_queue, true); // input queue sink [sizeof(float, input_queue, preserve index after = true)]
   gr::router::queue_source::sptr input_queue_source = gr::router::queue_source::make(sizeof(float), input_queue, true, false); // input queue source [sizeof(float), input_queue, preserve index = true, order = true]
 
-  GR_LOG_INFO(d_logger, "3. Finished building input queue sink/source");
+  /*
+  * Output queue Sink: Takes streams from the flow graph, packetized, slaps on an index, and pushes the result into the output queue; last argument indicates if the index is to be preserved from the stream tags
+  * Output queue Source: Grabs windows from the output queue, depacketizes, pulls out the index, and streams through flowgraph; arguments: preserve index (stream tags), order (guarrantee order of windows befoe streaming)
+  */
 
   gr::router::queue_sink::sptr output_queue_sink = gr::router::queue_sink::make(sizeof(float), output_queue, true);
   gr::router::queue_source::sptr output_queue_source = gr::router::queue_source::make(sizeof(float), output_queue, true, false);
 
-  GR_LOG_INFO(d_logger, "4. Finished building output queue sink/source");
-
+  /*
+  * Handler Code
+  * Chain of 50 FFTs that processes a window of 1024 values from the input_queue, and dumps the resulting output window into the output_queue
+  */
 
   std::vector<fft_ifft_sptr> ffts;
   for(int i = 0; i < fft_count; i++){
@@ -91,11 +89,10 @@ int main(int argc, char **argv)
   		tb->connect(ffts[i-1], 0, ffts[i], 0);
   	}
   }
-
   tb->connect(ffts.at(ffts.size()-1), 0, output_queue_sink, 0);
 
-  GR_LOG_INFO(d_logger, "5. Finished connecting all blocks.");
 
+  // Run flowgraph
   tb->run();
 
   // Exit normally.
--- apps/router_fft_test_parent.cc
+++ apps/router_fft_test_parent.cc
@@ -36,77 +36,86 @@ using namespace gr;
 int main(int argc, char **argv)
 {
 
+  /*
+  * Create a Top Block for the flowgraph
+  */
   gr::top_block_sptr tb_1 = gr::make_top_block("fft_ifft_parent");
 
   int window_size = 1024;
   int fft_count = 50;
   const char* in_file_name = "inputs/out.wav";
-  const char* out_file_name = "tr_fft_2_unordered_queue_cpp";
+  const char* out_file_name = "output.out";
+
+
+  /*
+  * Input wavfile source for streaming data into the flowgraph
+  * Output file sink for streaming output data to a file
+  */
 
   // Wavfile source to read in the input WAV file
   gr::blocks::wavfile_source::sptr wavfile_source = gr::blocks::wavfile_source::make(in_file_name, false); // input file source (WAV) [input_file, repeat=false]
   gr::blocks::file_sink::sptr file_sink = gr::blocks::file_sink::make(sizeof(float), out_file_name); // output file sink (BIN) [sizeof(float), output_file]
+
+  /*
+  * Input and Output queues to hold 'windows' of 1025 floats. 1 index, and 1024 samples
+  */
 
   //boost::shared_ptr<boost::lockfree::queue< std::vector<float>* > > input_queue(new boost::lockfree::queue<std::vector<float>* >(0)); // input queue
   //boost::shared_ptr<boost::lockfree::queue< std::vector<float>* > > output_queue(new boost::lockfree::queue<std::vector<float>* >(0)); // output queue
   boost::lockfree::queue< std::vector<float>* > input_queue(1025);
   boost::lockfree::queue< std::vector<float>* > output_queue(1025);
 
-  gr::router::root::sptr root_router = gr::router::root::make(1, input_queue, output_queue);
+  /*
+  * Load Balancing Router to distribute windows to children
+  */
 
-  //GR_LOG_DEBUG(d_debug_logger, "Creating input_queue_sink");
+  gr::router::root::sptr root_router = gr::router::root::make(1, input_queue, output_queue); // parent router [1 child, input queue, output queue]
   
+  /*
+  * Input queue Sink: Takes streams from a flow graph, packetizes, slaps on an index, and pushes the result into the input queue; last argument indicates if index is to be preserved from the stream tags
+  * Input queue Source: Grabs windows from the input queue, depacketizes, pulls out the index, and streams through flowgraph; arguments: preserve index (stream tags), order (guarantee order of windows before streaming)
+  */
+
   //tb_1->connect(wavfile_source, 0, file_sink, 0);
   gr::router::queue_sink::sptr input_queue_sink = gr::router::queue_sink::make(sizeof(float), input_queue, false); // input queue sink [sizeof(float, input_queue, preserve index after = true)]
-  	// There is no index to preserve! Start from 0
-  gr::router::queue_source::sptr input_queue_source = gr::router::queue_source::make(sizeof(float), input_queue, false, false); // input queue source [sizeof(float), input_queue, preserve index = true, order = true]
+  gr::router::queue_source::sptr input_queue_source = gr::router::queue_source::make(sizeof(float), input_queue, true, false); // input queue source [sizeof(float), input_queue, preserve index = true, order = true]
 
-  gr::router::queue_sink::sptr output_queue_sink = gr::router::queue_sink::make(sizeof(float), output_queue, false);
-  gr::router::queue_source::sptr output_queue_source = gr::router::queue_source::make(sizeof(float), output_queue, false, false);
+  /*
+  * Output queue Sink: Takes streams from the flow graph, packetized, slaps on an index, and pushes the result into the output queue; last argument indicates if the index is to be preserved from the stream tags
+  * Output queue Source: Grabs windows from the output queue, depacketizes, pulls out the index, and streams through flowgraph; arguments: preserve index (stream tags), order (guarrantee order of windows befoe streaming)
+  */
+
+  gr::router::queue_sink::sptr output_queue_sink = gr::router::queue_sink::make(sizeof(float), output_queue, true);
+  gr::router::queue_source::sptr output_queue_source = gr::router::queue_source::make(sizeof(float), output_queue, false, true); // Riht now out of order; need to reorder
 
   tb_1->connect(wavfile_source, 0, input_queue_sink, 0);
-  //std::cout << "Connected Wavfile_source to input_queue_sink" << std::endl;
-  
-  //tb_1->connect(input_queue_source, 0, file_sink, 0);
-  //tb_1->connect(input_queue_source, 0, output_queue_sink, 0);
-  //tb_1->connect(output_queue_source, 0, file_sink, 0);
-  
-  
+
+  /*
+  * Handler Code
+  * Chain of 50 FFTs that processes a window of 1024 values from the input_queue, and dumps the resulting output window into the output_queue
+  */
+
   std::vector<fft_ifft_sptr> ffts;
   for(int i = 0; i < fft_count; i++){
   	ffts.push_back(fft_ifft_make(1024));
   	if(i == 0){
   		tb_1->connect(input_queue_source, 0, ffts.at(0), 0);
-  	 //tb_1->connect(wavfile_source, 0, ffts.at(0), 0);
     }
   	else{
   		tb_1->connect(ffts.at(i-1), 0, ffts.at(i), 0);
   	}
   }
-
-  std::cout << "Connected chain of FFT/IFFTs" << std::endl;
-
   tb_1->connect(ffts.at(ffts.size()-1), 0, output_queue_sink, 0);
-  
 
-  //tb_1->connect(ffts.at(ffts.size()-1), 0, file_sink, 0);
-  //std::cout << "Connected FFT chain to output_queue_sink" << std::endl;
-  
-  tb_1->connect(output_queue_source, 0, file_sink, 0);
-  //std::cout << "Connected output_queue_source to file_sink" << std::endl;
-  tb_1->run();
-
-/*
-  std::vector<float> *temp;
-  for(int i = 0; i < 200; i++){
-  	input_queue.pop(temp);
-  	for(int j = 0; j < 1025; j++){
-  		std::cout << temp->at(j);
-  	}
-  	std::cout << std::endl;
-  }
+  /*
+  * Sink Code
+  * Connects the output_queue to the sink
   */
-  // Exit normally.
+
+  tb_1->connect(output_queue_source, 0, file_sink, 0);
+
+  // Run flowgraph
+  tb_1->run();
 
   return 0;
 }
