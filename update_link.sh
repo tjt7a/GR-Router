5a8ee2291397c9eae098519031c8f37972865c51
tjt7a <tjt7a@protoss.(none)>
2013-12-18 16:49:31 -0500

    Added update_link shell script to copy updated link paths

---
+++ update_link.sh
@@ -0,0 +1,3 @@
+cp test_fft_link.txt build/apps/CMakeFiles/fft_ifft_test.dir/link.txt
+cp test_parent_link.txt build/apps/CMakeFiles/router_fft_test_parent.dir/link.txt
+cp test_child_link.txt build/apps/CMakeFiles/router_fft_test_child.dir/link.txt
