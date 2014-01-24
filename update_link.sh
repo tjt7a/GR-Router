6c0c2fa7257db22e63e8d3c3ee213aa1ce19028e
Tom Tracy II <tjt7a@zerg.(none)>
2014-01-24 11:38:11 -0500

    Updated linking for WYSE boxes

--- update_link.sh
+++ update_link.sh
@@ -1,3 +1,4 @@
 cp test_fft_link.txt build/apps/CMakeFiles/fft_ifft_test.dir/link.txt
 cp test_parent_link.txt build/apps/CMakeFiles/router_fft_test_parent.dir/link.txt
 cp test_child_link.txt build/apps/CMakeFiles/router_fft_test_child.dir/link.txt
+cp transparent_link.txt build/apps/CMakeFiles/transparent.dir/link.txt
