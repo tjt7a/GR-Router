e74379936b7dc61739ddfa2cd6de76d8269130dd
tjt7a <tjta@virginia.edu>
2014-04-02 14:29:18 -0400

    Cleaned up the parent and child code and added more useful comments

--- apps/router_fft_test_child.cc
+++ apps/router_fft_test_child.cc
@@ -21,8 +21,8 @@
 
  /*
   This is a test application for the GR-ROUTER library.
-  An input WAV file is read, and the resulting samples are streamed through a series of FFT/IFFT blocks.
-  GR-ROUTER distributes the data out to multiple nodes to increase throughput.
+  A parent router sends messages to the child router, which then computes the FFT/IFFTx50 of that data
+  The child router then sends the output messages back to the parent.
 
 */
 
@@ -64,38 +64,38 @@ int main(int argc, char **argv)
   if(argc > 1)
   	parent_name = argv[1];
 
+  // Set the index of this child
   if(argc > 2)
     child_index = atoi(argv[2]);
 
   /*
-  * Input and Output queues to hold 'windows' of 1025 floats. 1 index, and 1024 samples
+  * Input and Output queues to hold vector 'message' addresses
   */
 
   boost::lockfree::queue< std::vector<float>*, boost::lockfree::fixed_sized<true> > input_queue(100);
   boost::lockfree::queue< std::vector<float>*, boost::lockfree::fixed_sized<true> > output_queue(100);
 
   /*
-  * Load Balancing Router to receive windows from parent
+  * Load Balancing Router to receive windows from parent; push workload into input_queue, and pop results from output_queue; last argument is not used at this time
   */
 
   gr::router::child::sptr child_router = gr::router::child::make(0, child_index, parent_name, input_queue, output_queue, 1e6);
 
   /*
-  * Input queue Sink: Takes streams from a flow graph, packetizes, slaps on an index, and pushes the result into the input queue; last argument indicates if index is to be preserved from the stream tags
   * Input queue Source: Grabs windows from the input queue, depacketizes, pulls out the index, and streams through flowgraph; arguments: preserve index (stream tags), order (guarantee order of windows before streaming)
   */
 
-  //gr::router::queue_sink::sptr input_queue_sink = gr::router::queue_sink::make(sizeof(float), input_queue, false); // input queue sink [sizeof(float, input_queue, preserve index after = true)]
   gr::router::queue_source::sptr input_queue_source = gr::router::queue_source::make(sizeof(float), input_queue, false, false); // input queue source [sizeof(float), input_queue, preserve index = true, order = true]
 
   /*
-  * Output queue Sink: Takes streams from the flow graph, packetized, slaps on an index, and pushes the result into the output queue; last argument indicates if the index is to be preserved from the stream tags
-  * Output queue Source: Grabs windows from the output queue, depacketizes, pulls out the index, and streams through flowgraph; arguments: preserve index (stream tags), order (guarrantee order of windows befoe streaming)
+  * Output queue Sink: Takes streams from the flow graph, packetized, slaps on headers, and pushes the result into the output queue; last argument indicates if the index is to be preserved from the stream tags
   */
 
   gr::router::queue_sink::sptr output_queue_sink = gr::router::queue_sink::make(sizeof(float), output_queue, false); // Construct from stream index?
-  //gr::router::queue_source::sptr output_queue_source = gr::router::queue_source::make(sizeof(float), output_queue, false, false, false);
 
+  /*
+  * Throughput: This block prints out the throughput of the Flowgraph in MegaSamples per second; first argument is the number of work() functions to be called between std::cout statements; the second argument is the index of the throughput block
+  */
 
   gr::router::throughput::sptr throughput = gr::router::throughput::make(sizeof(float), 10, 0);
 
@@ -116,6 +116,13 @@ int main(int argc, char **argv)
   		tb->connect(ffts.at(i-1), 0, ffts.at(i), 0);
   	}
   } 
+
+
+  /*
+    * Sink Code
+    * Connects the FFT/IFFT chain to a throughput block to get the throughput of the system, and then pass the results to the output file (/dev/null)
+  */
+
   tb->connect(ffts.at(ffts.size()-1), 0, throughput, 0);
 	
   tb->connect(throughput, 0, output_queue_sink, 0);
